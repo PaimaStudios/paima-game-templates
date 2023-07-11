eaa466dd340284837d0649441ce3fb87d810d3fd
Lukas Miskovsky <miskovskylukas@gmail.com>
2023-07-11 11:12:33 +0200

    Add nft & native nft sale proxy contract functions

--- nft-lvlup/frontend-nft-sale/src/services/constants.ts
+++ nft-lvlup/frontend-nft-sale/src/services/constants.ts
@@ -8,12 +8,5 @@ export const CHAIN_CURRENCY_DECIMALS: number = parseInt(
   process.env.CHAIN_CURRENCY_DECIMALS ?? '18'
 );
 
-if (!process.env.NATIVE_NFT_SALE_PROXY) {
-  console.error(
-    'NATIVE_NFT_SALE_PROXY is not set. Please fill in your .env file based on your contract deployment.'
-  );
-}
 export const NATIVE_NFT_SALE_PROXY: string = process.env.NATIVE_NFT_SALE_PROXY ?? '';
-
-// Contract address from the extensions.yml example, purely informational
-export const NFT = '0xb1D0c3142Ef25d7A693bD60b6892a0Eea27eFAf0';
+export const NFT: string = process.env.NFT ?? '';
