6f0f5da47dcf6cffa4db439f47978f6f386ff7f3
Edward Alvarado <edward@cotalker.com>
2023-09-13 08:11:20 -0300

    fallback

--- chess/frontend/src/pages/ChessGame/ChessBoard.tsx
+++ chess/frontend/src/pages/ChessGame/ChessBoard.tsx
@@ -31,7 +31,13 @@ const ChessBoard: React.FC<Props> = ({
   const palette = useTheme().palette.secondary;
   const game = new Chess(board);
   const getWidth = () => {
-    const responsiveSize = document.documentElement.clientWidth - PADDING;
+    // document.documentElement.clientWidth works better than window.innerWidth, generally, in mobile.
+    // if both available, use the smaller one.
+    // window.screen.width doesn't account for rotation, so just use as final backup.
+    const width = window.innerWidth && document.documentElement.clientWidth ? 
+        Math.min(window.innerWidth, document.documentElement.clientWidth) : 
+        document.documentElement.clientWidth || window.innerWidth || window.screen.width;
+    const responsiveSize = width - PADDING;
     return Math.max(100, Math.min(MAX_BOARD_WIDTH, responsiveSize));
   }
 
