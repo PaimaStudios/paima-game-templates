1e0a6ec8943de8d5786a51556a3b05302e8d93ec
Sebastien Guillemot <SebastienGllmt@gmail.com>
2023-10-03 06:14:54 +0800

    Fix middleware types (#42)

--- chess/middleware/scripts/build.sh
+++ chess/middleware/scripts/build.sh
@@ -4,4 +4,6 @@ cd middleware
 
 DOTENV_CONFIG_PATH=../../.env.$NODE_ENV npx -c ../node_modules/paima-sdk/node_modules/.bin/paima-build-middleware
 
-cp packaged/middleware.js ../frontend/src/paima/middleware.js
+cp packaged/middleware.js $OUT/middleware.js
+rsync -ar --include='*/' --include='*.d.ts' --exclude='*' build/ $OUT/
+mv $OUT/index.d.ts $OUT/middleware.d.ts
