723ee2b82aca04595992d9a68d7d0a3852f3d694
Sebastien Guillemot <sebastiengllmt@gmail.com>
2024-01-19 10:45:11 +0900

    Update hex-battle to 2.2.0

--- hex-battle/backend/middleware/scripts/build.sh
+++ hex-battle/backend/middleware/scripts/build.sh
@@ -4,8 +4,8 @@ cd ..
 npm run build
 cd middleware
 
-: "${NODE_ENV:=development}"
-DOTENV_CONFIG_PATH=../../.env.${NODE_ENV:-development} npx paima-build-middleware
+: "${NETWORK:=localhost}"
+DOTENV_CONFIG_PATH=../../.env.${NETWORK:-localhost} npx paima-build-middleware
 
 OUT=../../frontend/src/paima
 rm -rf $OUT/
