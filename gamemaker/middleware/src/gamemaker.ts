153f5b38b12c416539ec274a4901cf193a093d7b
Tad Hardesty <tad@dcspark.io>
2024-04-24 15:41:46 -0700

    Use unknown instead of any, fix null handling

--- gamemaker/middleware/src/gamemaker.ts
+++ gamemaker/middleware/src/gamemaker.ts
@@ -5,26 +5,24 @@ const GM_TO_JS: ProxyHandler<object> = {
   get(target, p, receiver) {
     return gmToJs(Reflect.get(target, 'gml' + String(p), receiver));
   },
-  set(target, p, newValue, receiver) {
-    return gmToJs(Reflect.set(target, 'gml' + String(p), newValue, receiver));
+  set(target, p, newValue, receiver): boolean {
+    return Reflect.set(target, 'gml' + String(p), newValue, receiver);
   },
 };
 /**
  * Wrap a GameMaker object or function transparently for JS.
- * @param obj
- * @returns
  */
-function gmToJs(obj: any): any {
+function gmToJs(obj: unknown): unknown {
   if (obj instanceof Function) {
     const name = `${gmToJs.name}(${obj.name})`;
     return {
       // Ritual to let our dynamic name show up in debuggers.
-      [name](...args: any[]) {
+      [name](...args: unknown[]) {
         // Supply null for first two _inst and _other args that GM always expects.
         return gmToJs(obj(null, null, ...args.map(jsToGm)));
       }
     }[name];
-  } else if (typeof obj === 'object') {
+  } else if (obj !== null && typeof obj === 'object') {
     return new Proxy(obj, GM_TO_JS);
   } else {
     return obj;
@@ -40,28 +38,26 @@ const JS_TO_GM: ProxyHandler<object> = {
       return true;
     return jsToGm(Reflect.get(target, String(p).replace(/^gml/, ''), receiver), target);
   },
-  set(target, p, newValue, receiver) {
-    return jsToGm(Reflect.set(target, String(p).replace(/^gml/, ''), newValue, receiver), target);
+  set(target, p, newValue, receiver): boolean {
+    return Reflect.set(target, String(p).replace(/^gml/, ''), newValue, receiver);
   },
 };
 /**
  * Wrap a JS object or function transparently for GameMaker.
- * @param obj
- * @param receiver The receiver to bind to, if {@link obj} is a function.
- * @returns
+ * @param target The receiver to bind to, if {@link obj} is a function.
  */
-export function jsToGm(obj: any, target?: any): any {
+export function jsToGm(obj: unknown, target?: unknown): unknown {
   if (obj instanceof Function) {
     // JS functions must be pre-bound to their receiver.
     const name = `${jsToGm.name}(${obj.name})`;
     return {
       // Ritual to let our dynamic name show up in debuggers.
-      [name](_inst: any, _other: any, ...args: any[]) {
+      [name](_inst: unknown, _other: unknown, ...args: unknown[]) {
         // Ignore first two _inst and _other args that GM always adds.
         return jsToGm(obj.apply(target, args.map(gmToJs)) /* no target here */);
       }
     }[name];
-  } else if (typeof obj === 'object') {
+  } else if (obj !== null && typeof obj === 'object') {
     return new Proxy(obj, JS_TO_GM);
   } else {
     return obj;
