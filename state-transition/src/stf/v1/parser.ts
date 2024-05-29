c294f7ee547a41af12aa91a40e8387840f5b3192
Tad Hardesty <tad@dcspark.io>
2024-05-29 16:41:43 -0700

    Add files from Paima generic template

---
+++ state-transition/src/stf/v1/parser.ts
@@ -0,0 +1,29 @@
+import type { InvalidInput } from '@game/utils';
+import { PaimaParser } from '@paima/sdk/concise';
+import type { ParsedSubmittedInput } from './types';
+
+const myGrammar = `gainedExperience = xp|*address|experience`;
+
+const parserCommands = {
+  gainedExperience: {
+    address: PaimaParser.WalletAddress(),
+    experience: PaimaParser.NumberParser(1, 5),
+  },
+};
+
+const myParser = new PaimaParser(myGrammar, parserCommands);
+export function isInvalid(input: ParsedSubmittedInput): input is InvalidInput {
+  return (input as InvalidInput).input == 'invalidString';
+}
+
+function parse(s: string): ParsedSubmittedInput {
+  try {
+    const parsed = myParser.start(s);
+    return { input: parsed.command, ...parsed.args } as any;
+  } catch (e) {
+    console.log(e, 'Parsing error');
+    return { input: 'invalidString' };
+  }
+}
+
+export default parse;
