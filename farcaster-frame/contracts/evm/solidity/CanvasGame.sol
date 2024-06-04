f87c4ada17b21ea74da3fbc96083cf08cdcecf2c
Tad Hardesty <tad@dcspark.io>
2024-06-04 07:14:27 -0700

    Address Solidity review

--- farcaster-frame/contracts/evm/solidity/CanvasGame.sol
+++ farcaster-frame/contracts/evm/solidity/CanvasGame.sol
@@ -2,26 +2,32 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract CanvasGame is Ownable {
-    uint256 public fee = 0.00002651 ether; // about $0.10 USD at time of writing
-    uint256 public paintLimit = 48;
+    uint256 constant public fee = 0.00002651 ether; // about $0.10 USD at time of writing
+    uint256 constant public paintLimit = 48;
 
     address[] private canvasOwner;
-    mapping(uint256 => uint256) private paintCount;
-    mapping(uint256 => mapping(address => bool)) private painted;
-    mapping(uint256 => mapping(address => bool)) private forked;
+    mapping(uint256 canvasId => uint256 paintCount) private paintCount;
+    mapping(uint256 canvasId => mapping(address user => bool hasPainted)) private painted;
+    mapping(uint256 canvasId => mapping(address user => bool hasForked)) private forked;
 
     mapping(address => uint256) public rewards;
 
     event NewCanvas(address indexed canvasOwner, uint256 id, uint256 copyFrom);
     event Paint(address indexed contributor, uint256 canvas, uint24 color);
 
+    error InsufficientFee(uint256 got, uint256 wanted);
+    error CanvasDoesNotExist(uint256 requested, uint256 max);
+    error CanvasFull(uint256 canvas, uint256 paintLimit);
+    error InvalidFork(uint256 canvas);
+
     constructor(address initialOwner)
         Ownable(initialOwner)
     {
         // Generate 16 seed canvases. If id == copyFrom, it's a seed canvas.
-        for (uint256 id = 0; id < 16; ++id) {
+        for (uint256 id; id < 16; ++id) {
             _newCanvas(initialOwner, id);
         }
     }
@@ -33,9 +39,9 @@ contract CanvasGame is Ownable {
     }
 
     function paint(uint256 canvas, uint24 color) public payable {
-        require(msg.value >= fee, "Sufficient fee required to paint");
-        require(canvas < canvasOwner.length, "Canvas ID must exist");
-        require(paintCount[canvas] < paintLimit, "Canvas must not be full");
+        if (!(msg.value >= fee)) revert InsufficientFee(msg.value, fee); // Sufficient fee required to paint
+        if (!(canvas < canvasOwner.length)) revert CanvasDoesNotExist(canvas, canvasOwner.length); // Canvas ID must exist
+        if (!(paintCount[canvas] < paintLimit)) revert CanvasFull(canvas, paintLimit); // Canvas must not be full
 
         uint256 contractOwnerReward = msg.value / 10;
         uint256 canvasOwnerReward = msg.value - contractOwnerReward;
@@ -43,16 +49,16 @@ contract CanvasGame is Ownable {
         rewards[canvasOwner[canvas]] += canvasOwnerReward;
 
         painted[canvas][msg.sender] = true;
-        paintCount[canvas] += 1;
+        ++paintCount[canvas];
 
         emit Paint(msg.sender, canvas, color);
     }
 
     function fork(uint256 canvas) public {
-        require(canvas < canvasOwner.length, "Canvas ID must exist");
-        require(painted[canvas][msg.sender], "Must have painted to this canvas before");
-        require(canvasOwner[canvas] != msg.sender, "Must not fork your own canvas");
-        require(!forked[canvas][msg.sender], "Must not have forked this canvas already");
+        if (!(canvas < canvasOwner.length)) revert CanvasDoesNotExist(canvas, canvasOwner.length); // Canvas ID must exist
+        if (!(painted[canvas][msg.sender])) revert InvalidFork(canvas); // Must not have painted to this canvas before
+        if (!(canvasOwner[canvas] != msg.sender)) revert InvalidFork(canvas); // Must not fork your own canvas
+        if (!(!forked[canvas][msg.sender])) revert InvalidFork(canvas); // Must not have forked this canvas already
         forked[canvas][msg.sender] = true;
         _newCanvas(msg.sender, canvas);
     }
@@ -60,7 +66,9 @@ contract CanvasGame is Ownable {
     function withdraw() public {
         require(rewards[msg.sender] > 0, "Nothing to withdraw");
         uint256 amount = rewards[msg.sender];
-        payable(msg.sender).transfer(amount);
         delete rewards[msg.sender];
+
+        // Library reverts on failure. Must be last to avoid reentrancy vulnerability.
+        Address.sendValue(payable(msg.sender), amount);
     }
 }
