7714f44f211bf4dabbdd39414b090aea5c246830
Tad Hardesty <tad@dcspark.io>
2024-07-09 13:06:25 -0700

    Add OPFS cache

--- mina/frontend/worker.js
+++ mina/frontend/worker.js
@@ -1,5 +1,21 @@
 import endpoints from './build/middleware.js';
 
+function future() {
+  let resolve;
+  return [new Promise(r => resolve = r), resolve];
+}
+
+const [signaturePromise, signatureResolve] = future();
+
+self.addEventListener('message', e => {
+  if ('signature' in e.data) {
+    signatureResolve(e.data.signature);
+  }
+});
+
 postMessage({ status: 'Compiling' });
 await endpoints.compile();
-postMessage({ status: 'All done' });
+postMessage({ status: 'Waiting for your signature' });
+const signature = await signaturePromise;
+postMessage({ status: 'Proving' });
+await endpoints.prove(signature);
--- mina/middleware/src/index.ts
+++ mina/middleware/src/index.ts
@@ -11,9 +11,54 @@ import { AddressType } from '@paima/sdk/utils';
 initMiddlewareCore(GAME_NAME, gameBackendVersion);
 
 import { EvmInjectedConnector } from '@paima/providers';
-import { PrivateKey, PublicKey } from 'o1js';
+import { Cache, CacheHeader, PrivateKey, PublicKey } from 'o1js';
 
 const temporaryPrivateKey = PrivateKey.random();
+
+function openDb(name: string, version?: number) {
+  return new Promise((resolve, reject) => {
+    var request = indexedDB.open(name, version);
+    request.onerror = e => reject(e);
+    request.onsuccess = e => resolve(request.result);
+  });
+}
+
+class IndexedDbCache implements Cache {
+  canWrite: boolean = true;
+  debug?: boolean | undefined;
+
+  files = new Map<string, Uint8Array>();
+  dir?: FileSystemDirectoryHandle;
+
+  async load() {
+    const root = await navigator.storage.getDirectory();
+    this.dir = await root.getDirectoryHandle('zkcache', { create: true });
+    for await (const [k, v] of this.dir.entries()) {
+      if (v instanceof FileSystemFileHandle) {
+        console.log('load', k);
+        this.files.set(k, new Uint8Array(await (await v.getFile()).arrayBuffer()));
+      }
+    }
+  }
+
+  read(header: CacheHeader): Uint8Array | undefined {
+    console.log('read', header.persistentId, this.files.has(header.persistentId));
+    return this.files.get(header.persistentId);
+  }
+  write(header: CacheHeader, value: Uint8Array): void {
+    this.files.set(header.persistentId, value);
+    (async () => {
+      const file = await this.dir!.getFileHandle(header.persistentId, { create: true });
+      //if (!file) throw new Error('!file');
+      const writeable = await file.createWritable();
+      //if (!writeable) throw new Error('!writeable');
+      await writeable.write(value);
+      await writeable.close();
+      console.log('write', header.persistentId, 'ok');
+    })();
+  }
+}
+const cache = new IndexedDbCache();
 
 const endpoints = {
   ...paimaEndpoints,
@@ -21,7 +66,14 @@ const endpoints = {
   ...writeEndpoints,
 
   async compile() {
-    await DelegationOrderProgram.compile();
+    if (cache.files.size == 0) {
+      console.time('cache.load');
+      await cache.load();
+      console.timeEnd('cache.load');
+    }
+    console.time('DelegationOrderProgram.compile');
+    await DelegationOrderProgram.compile({ cache });
+    console.timeEnd('DelegationOrderProgram.compile');
   },
 
   async sign() {
