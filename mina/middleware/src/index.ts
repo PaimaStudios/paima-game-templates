372acaf3258689a3f3f51a253cd38b652bdcf669
Tad Hardesty <tad@dcspark.io>
2024-07-09 10:17:38 -0700

    Use new middleware to sign binary DelegationOrder

--- mina/middleware/src/index.ts
+++ mina/middleware/src/index.ts
@@ -1,13 +1,19 @@
-import { paimaEndpoints } from '@paima/sdk/mw-core';
+import { paimaEndpoints, WalletConnectHelper } from '@paima/sdk/mw-core';
 import { queryEndpoints } from './endpoints/queries';
 import { writeEndpoints } from './endpoints/write';
 
 import { initMiddlewareCore } from '@paima/sdk/mw-core';
 
 import { gameBackendVersion, GAME_NAME } from '@game/utils';
-import { DelegationOrder, DelegationOrderProgram } from '@game/mina-contracts';
+import { DelegationOrder, DelegationOrderProgram, Secp256k1 } from '@game/mina-contracts';
+import { AddressType } from '@paima/sdk/utils';
 
 initMiddlewareCore(GAME_NAME, gameBackendVersion);
+
+import { EvmInjectedConnector } from '@paima/providers';
+import { PrivateKey, PublicKey } from 'o1js';
+
+const temporaryPrivateKey = PrivateKey.random();
 
 const endpoints = {
   ...paimaEndpoints,
@@ -16,7 +22,13 @@ const endpoints = {
 
   async compile() {
     await DelegationOrderProgram.compile();
-  }
+  },
+
+  async sign() {
+    const provider = await EvmInjectedConnector.instance().connectSimple({ gameName: GAME_NAME, gameChainId: undefined });
+    const signature = await provider.signMessage(DelegationOrder.bytesToSign(temporaryPrivateKey.toPublicKey()));
+    console.log('signature', signature);
+  },
 };
 
 export * from './types';
