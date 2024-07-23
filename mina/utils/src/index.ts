323a996cf084cf4423b21ad902af3d1eb0c33351
Tad Hardesty <tad@dcspark.io>
2024-07-22 17:37:38 -0700

    Revert "Click & Moo demo assets"
    This reverts commit 99c2a952f9efde758b21fda97a19e744c15bc34b.

--- mina/utils/src/index.ts
+++ mina/utils/src/index.ts
@@ -3,7 +3,7 @@ const VERSION_MAJOR = 1;
 const VERSION_MINOR = 1;
 const VERSION_PATCH = 1;
 export const gameBackendVersion: VersionString = `${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}`;
-export const GAME_NAME = 'Click & Moo';
+export const GAME_NAME = 'Generic Game';
 export const PRACTICE_BOT_ADDRESS = '0x0101';
 
 export * from './types.js';
